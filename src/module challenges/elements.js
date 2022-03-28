36d1d251f3a598ecb4a6239d9f583ae0e2a24627
Maneesha M <mane3012@global.chainsys.com>
2022-03-28 20:37:32 +0530

    index

--- src/module challenges/elements.js
+++
@@ -1,8 +0,0 @@
-import { myFunction } from "./money.js";
-import { getOptions } from "./handlrers.js";
-import { currencies } from "./currencies.js";
-const primaryCurrency = document.getElementById('primary');
-const secondaryCurrency = document.getElementById('secondary');
-primaryCurrency.innerHTML = getOptions(currencies);
-secondaryCurrency.innerHTML = getOptions(currencies);
-document.getElementById('amount').addEventListener('change', myFunction);
--- src/module challenges/money.js
+++
@@ -1,5 +0,0 @@
-export function myFunction() {
-    var currency = document.getElementById('amount');
-    var result =parseInt((Math.ceil(Math.random() * 100)) * currency.value);
-    document.getElementById('result').innerHTML = result;
-  }
